823467408552a4ec723fd8f421bdac454f15cdd4
Willigan49 <guillaume.gernigon@outlook.fr>
2023-07-14 16:58:53 +0200

    test classname

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -5,14 +5,16 @@ type Props = {
   options: Array<Object>;
   style?: CSSProperties;
   onChange: ChangeEventHandler<HTMLSelectElement>;
+  className?: string,
 };
 
-export default function Select({ name, options, style, onChange }: Props): JSX.Element {
+export default function Select({ name, options, style, onChange, className }: Props): JSX.Element {
   return (
     <select
       style={style}
       id={`${name}-select`}
       onChange={onChange}
+      className={className}
     >
       {options.map((option: any) => (
         <option key={option.id} value={option.name}>
