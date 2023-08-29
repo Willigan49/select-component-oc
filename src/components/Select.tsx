e8c0de39f9223999d104f025d1ac79b714ae44ad
Willigan49 <guillaume.gernigon@outlook.fr>
2023-08-29 18:08:12 +0200

    add doc

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -7,7 +7,15 @@ type Props = {
   onChange: ChangeEventHandler<HTMLSelectElement>;
   className?: string,
 };
-
+/**
+ * Functionnal componant with select list.
+ * @param name string for id of the select
+ * @param options array of object with the different options
+ * @param style css propperties for style 
+ * @param onChange function called by the change event of the select
+ * @param className string for the class
+ * @returns select componant
+ */
 export default function Select({ name, options, style, onChange, className }: Props): JSX.Element {
   return (
     <select
